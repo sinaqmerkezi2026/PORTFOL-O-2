d2a3d7efffa9c92fcdd982cceb4953e43c1ee601
Nicat Hüseynli <huseynlinicat2010@gmail.com>
2026-05-08 17:57:10 +0000

    şəkiı deployda görsənmir url linki bu olsun https://instagram.fgyd12-1.f

--- next.config.ts
+++ next.config.ts
@@ -53,6 +53,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: '*.fbcdn.net',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
